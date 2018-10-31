eb69ea36e00aa1c25074623be0ee80539fb4d3ad
Tiago Prosp <tiago@prosp>
2018-10-31 17:14:52 +0000

    .

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ slimbot.startPolling()
 
 //Scanner
 const scanner = new Scanner()
-scanner.start_scanning({time: 900000})
+scanner.start_scanning({time: 120000})
 let PAIR_CACHE, AI_PAIR_CACHE
 let currencies = ['BTC', 'ETH', 'BNB', 'USDT']
 
