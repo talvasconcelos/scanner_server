956a1acccdb1e72cb1b77ef5cbd92e0b42715069
Tiago Vasconcelos <tvasconcelos@sedeprosp.pt>
2019-05-15 16:32:22 +0100

    new AI model on TPU

--- cryptohopper.js
+++ cryptohopper.js
@@ -14,7 +14,7 @@ tf.setBackend('cpu')
 // const json = tf.loadModel('https://market-scanner.herokuapp.com/lib/models/lstm-model.json')
 // const bin = tf.loadModel('https://market-scanner.herokuapp.com/lib/models/lstm-model.weights.bin')
 
-const model = tf.loadModel('http://tvasconcelos.eu/model/cms/model/model.json')
+const model = tf.loadModel('http://tvasconcelos.eu/model/cms/model_02/model.json')
 
 const API_KEY = process.env.HOPPER_KEY
 const API_SECRET = process.env.HOPPER_SECRET
